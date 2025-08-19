5c5b3588a1e22ef05c1f181428ee70d28ec211ab
Darekar Yogita <147601324+Yogita-darekar@users.noreply.github.com>
2025-08-19 14:58:17 +0530

    Add files via upload

---
+++ Datatypes/Test.java
@@ -0,0 +1,23 @@
+package com.tka.july3;
+
+public class Test {
+
+	public static void main(String[] args) {
+		defaultValue df = new defaultValue();
+		System.out.println(df.a);
+		System.out.println(df.f);
+		System.out.println(df.sh);
+		System.out.println(df.l);
+		System.out.println(df.d);
+		System.out.println(df.c);
+		System.out.println(df.b);
+		System.out.println(df.bln);
+		System.out.println(df.s);
+		System.out.println(df.mob);
+		System.out.println(df.num);
+		
+		 System.out.println("Integer.MAX_VALUE = "
+                 + Long.MAX_VALUE);
+	}
+
+}
---
+++ Datatypes/defaultValue.java
@@ -0,0 +1,16 @@
+package com.tka.july3;
+
+public class defaultValue {
+	int a;
+	float f;
+	short sh;
+	long l;
+	double d;
+	char c;
+	byte b;
+	boolean bln;
+	String s;
+	int mob = 1234567890;
+	long num = 1796447048;
+
+}
