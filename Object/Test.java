e5e763f4c3756c790cf35b799b3f7a6ca21637d2
Darekar Yogita <147601324+Yogita-darekar@users.noreply.github.com>
2025-08-20 17:53:46 +0530

    Add files via upload

---
+++ Object/Test.java
@@ -0,0 +1,17 @@
+package com.tka.july2;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Start of the main method");
+		Student s1 = new Student();
+		s1.m1();
+		System.out.println(s1);
+		Student s2 = new Student();
+		s2.m1();
+		System.out.println(s2);
+		System.out.println("End of the main method");
+
+	}
+
+}
