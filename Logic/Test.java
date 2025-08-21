edc37007fb45cb32cfee92bbd4aa14e279329c81
Darekar Yogita <147601324+Yogita-darekar@users.noreply.github.com>
2025-08-21 14:16:07 +0530

    Add files via upload

---
+++ Logic/Test.java
@@ -0,0 +1,21 @@
+package com.tka.july10;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Logic obj = new Logic();
+//		obj.primeOne(3);
+//		obj.primeTwo(9);
+//		obj.primeThree(17);
+//		obj.primeFour(19);
+//		obj.primeFive(31);
+//		obj.numOfDigits(1234);
+//		obj.reverseNum(45637);
+//        obj.sumofDigits(1234);
+//        obj.pallindrome(12321);
+//        obj.evenOdd(12);
+//        obj.swap(10, 20);
+        obj.powerOfNum(25);
+	}
+
+}
