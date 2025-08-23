f100ea907423c5d98c0adc854d5608bc809b68f7
Darekar Yogita <147601324+Yogita-darekar@users.noreply.github.com>
2025-08-23 21:47:35 +0530

    Add files via upload

---
+++ constructor/Test.java
@@ -0,0 +1,14 @@
+package com.tka.july11;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		Student s2 = new Student(5);
+		s2.roll = 3;
+		Student s3 = new Student(5,"jay");
+		Student s4 = new Student(5,"jay",78);
+
+	}
+
+}
