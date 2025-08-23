4c83ae96c332fc8496aef92f94a69c13256e97e5
Darekar Yogita <147601324+Yogita-darekar@users.noreply.github.com>
2025-08-23 21:40:43 +0530

    Add files via upload

---
+++ static/ATM.java
@@ -0,0 +1,18 @@
+package com.tka.july14;
+
+public class ATM {
+
+	public static void main(String[] args) {
+
+
+		Bank u1 = new Bank();
+		u1.withraw(500);
+
+		Bank u2 = new Bank();
+		u2.withraw(600);
+		
+		
+
+	}
+
+}
