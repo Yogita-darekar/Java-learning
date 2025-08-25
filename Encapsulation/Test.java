73a2717423c5e8abb31046da49aae18ce82e958f
Darekar Yogita <147601324+Yogita-darekar@users.noreply.github.com>
2025-08-25 18:07:49 +0530

    Add files via upload

---
+++ Encapsulation/Test.java
@@ -0,0 +1,18 @@
+package com.tka.july16;
+
+public class Test {
+
+	public static void main(String[] args) {
+		student s = new student();
+		System.out.println(s.getRoll());
+		s.setRoll(13);
+		System.out.println(s.getRoll());
+		System.out.println(s.getName());
+		s.setName("yogita");
+		System.out.println(s.getRoll());
+		
+		
+
+	}
+
+}
