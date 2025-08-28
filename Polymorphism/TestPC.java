50a0235a973653237effa8cb2aad1c16463c34bf
Darekar Yogita <147601324+Yogita-darekar@users.noreply.github.com>
2025-08-28 19:05:05 +0530

    Add files via upload

---
+++ Polymorphism/TestPC.java
@@ -0,0 +1,16 @@
+package com.tka.july21;
+
+public class TestPC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Parent  obj = new Child();
+		obj.property();
+		obj.marrige();
+	
+		
+
+	}
+
+}
