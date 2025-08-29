5a9e1987d5c5dd0a81cdb5b93afe3d53d8f97d47
Darekar Yogita <147601324+Yogita-darekar@users.noreply.github.com>
2025-08-29 22:46:01 +0530

    Add files via upload

---
+++ Super-and-This/Parent.java
@@ -0,0 +1,8 @@
+package com.tka.july24;
+
+public class Parent {
+	
+	int p = 100;
+
+	
+}
---
+++ Super-and-This/Test.java
@@ -0,0 +1,11 @@
+package com.tka.july24;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Child c = new Child();
+		c.m1();
+
+	}
+
+}
